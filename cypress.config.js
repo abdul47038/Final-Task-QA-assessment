adb5a24d0895ea19b2388dc0da68fd94242c8adb
Abdul <Abdulabbasi483@gmail.com>
2024-07-15 16:15:52 +0500

    add files

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+   projectId: "7jqhji",
+  
+  e2e: {
+
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+   
+   
+    },
+  },
+});
